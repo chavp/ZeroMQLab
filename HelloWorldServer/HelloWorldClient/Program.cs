7322760ee47085d849cb437a5004ad11196e328c
chavp <my.parinya@gmail.com>
2015-04-22 17:32:02 +0700

    add Reliable Request-Reply Patterns
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ HelloWorldServer/HelloWorldClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    using NetMQ;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = NetMQContext.Create())
+            using (var requester = ctx.CreateRequestSocket())
+            {
+                requester.Connect("tcp://localhost:5555");
+
+                while (true)
+                {
+                    Console.WriteLine("Request Hello\n");
+                    requester.Send("Hello");
+
+                    string m2 = requester.ReceiveString();
+                    Console.WriteLine("From Server: {0}", m2);
+
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            }
+        }
+    }
+}
---
+++ HelloWorldServer/HelloWorldServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldServer
+{
+    using NetMQ;
+    using ZeroMQ;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = NetMQContext.Create())
+            using (var responder = ctx.CreateResponseSocket())
+            {
+                responder.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    Console.WriteLine("Wait for Received Hello\n");
+                    string m1 = responder.ReceiveString();
+                    Console.WriteLine("Received Hello\n");
+
+                    responder.Send("World");
+                }
+            }
+        }
+    }
+}
